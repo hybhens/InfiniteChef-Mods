54cfc8dc2e1a8a9f103e8dbdbab89fd66ce2538d
brobgonal <97367267+hybhens@users.noreply.github.com>
2024-06-10 22:41:38 +0200

    Create Objects.js

---
+++ Objects.js
@@ -0,0 +1,56 @@
+addIngredient("Balls", {
+    color: "#8B4513",        // A brown color resembling cooked meatballs
+    cookColor: "#A0522D",    // A darker brown when cooked
+    type: "protein",         // Categorized as a protein ingredient
+    shape: "round",          // Balls are spherical
+    keywords: "round,meat,protein", // Keywords associated with Balls
+    adj: "juicy"             // Adjective describing the texture or flavor
+});
+addIngredient("Cubes", {
+    color: "#D2691E",        // A brownish-orange color, similar to uncooked beef cubes
+    cookColor: "#8B0000",    // A dark brownish-red when cooked, representing cooked beef cubes
+    type: "protein",         // Categorized as a protein ingredient
+    shape: "cube",           // Cubes are cubic in shape
+    keywords: "cube,meat,protein", // Keywords associated with Cubes
+    adj: "tender"            // Adjective describing the texture or flavor
+});
+addIngredient("Vegetable Cubes", {
+    color: "#32CD32",        // Green color for vegetable cubes
+    cookColor: "#228B22",    // Darker green when cooked
+    type: "vegetable",       // Categorized as a vegetable
+    shape: "cube",           // Cubic shape
+    keywords: "cube,vegetable", // Keywords for vegetable cubes
+    adj: "crispy"            // Adjective describing the texture
+});
+addIngredient("Cheese Cubes", {
+    color: "#FFD700",        // Yellow color for cheese cubes
+    cookColor: "#FFA500",    // Darker orange when cooked or melted
+    type: "dairy",           // Categorized as a dairy product
+    shape: "cube",           // Cubic shape
+    keywords: "cube,cheese,dairy", // Keywords for cheese cubes
+    adj: "creamy"            // Adjective describing the texture
+});
+addIngredient("Spikes", {
+    color: "#FF6347",        // A tomato red color for raw spikes
+    cookColor: "#CD5C5C",    // A darker red when cooked
+    type: "garnish",         // Categorized as a garnish
+    shape: "spiky",          // Describes a spiky shape
+    keywords: "spiky,sharp,garnish", // Keywords associated with Spikes
+    adj: "crunchy"           // Adjective describing the texture or flavor
+});
+addIngredient("Chocolate Spikes", {
+    color: "#8B4513",        // Brown color for chocolate spikes
+    cookColor: "#A0522D",    // Darker brown when cooked or melted
+    type: "dessert",         // Categorized as a dessert ingredient
+    shape: "spiky",          // Spiky shape
+    keywords: "spiky,chocolate,dessert", // Keywords for chocolate spikes
+    adj: "brittle"           // Adjective describing the texture
+});
+addIngredient("Veggie Spikes", {
+    color: "#32CD32",        // Green color for vegetable spikes
+    cookColor: "#228B22",    // Darker green when cooked
+    type: "vegetable",       // Categorized as a vegetable
+    shape: "spiky",          // Spiky shape
+    keywords: "spiky,vegetable", // Keywords for vegetable spikes
+    adj: "crispy"            // Adjective describing the texture
+});
